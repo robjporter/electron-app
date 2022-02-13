59f59db7d383b7147b611f2b70d74e5cd3a75498
robjporter <robporter79@outlook.com>
2022-02-13 21:34:55 +0000

    Fixed utils

--- src/main/util.ts
+++ src/main/util.ts
@@ -3,7 +3,8 @@ import { URL } from 'url';
 import path from 'path';
 
 export let resolveHtmlPath: (htmlFileName: string) => string;
-export const isDevelopment = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+export const isDevelopment =
+  process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 export const isProduction = process.env.NODE_ENV === 'production';
 
 if (isDevelopment) {
