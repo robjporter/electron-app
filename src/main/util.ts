d32cd40a58a19ff0e9044332751d50027dd55b14
robjporter <robporter79@outlook.com>
2022-02-13 21:23:32 +0000

    Utils correction

--- src/main/util.ts
+++ src/main/util.ts
@@ -3,7 +3,6 @@ import { URL } from 'url';
 import path from 'path';
 
 export let resolveHtmlPath: (htmlFileName: string) => string;
-
 export const isDevelopment = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 export const isProduction = process.env.NODE_ENV === 'production';
 
